d4c106c015c2b99bfc66f072308a73ee7657fc4d
ilucasbrandao <dev.lucasbrandao@gmail.com>
2026-09-04 11:28:16 -0300

    feat: adiciona atributos e annotations ao model Post.

--- src/main/java/com/dev/blog_pessoal/model/PostModel.java
+++ src/main/java/com/dev/blog_pessoal/model/PostModel.java
@@ -1,4 +1,35 @@
 package com.dev.blog_pessoal.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "db_post")
 public class PostModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    private OffsetDateTime dataCriacao;
+
+    @NotBlank
+    private String category;
+
 }
